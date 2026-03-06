1cd54ea1b69e0ef241976c87e571a0e154cce9a6
Stéphane Long <81955196+stephane-long@users.noreply.github.com>
2026-03-06 15:30:46 +0100

    Intégration sdk newsapi + fonction déduplication des sources

--- scripts/news_fetcher.py
+++ scripts/news_fetcher.py
@@ -1,36 +1,45 @@
-import requests
 import os
 import sys
 import json
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from newsapi import NewsApiClient
+
 
 load_dotenv()
-
-# Remplacez par votre clé NewsAPI
 NEWSAPI_API_KEY = os.getenv("NEWSAPI_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY", "")
 
 
-def search_news(query, days=1):
+def deduplicate_sources(sources):
+    seen_url = set()
+    unique = []
+
+    for source in sources:
+        url = source.get("url", "")
+        if url and url not in seen_url:
+            unique.append(url)
+            seen_url.add(url)
+    return unique
+
+
+def search_newsapi(query, days=1):
     # Calcul de la date de début
     from_date = (datetime.now() - timedelta(days=int(days))).strftime("%Y-%m-%d")
 
-    url = "https://newsapi.org/v2/everything"
-    params = {
-        "q": query,
-        "from": from_date,
-        "sortBy": "relevancy",
-        "language": "fr",
-        "apiKey": NEWSAPI_API_KEY,
-    }
+    newsapi = NewsApiClient(api_key=NEWSAPI_API_KEY)
 
     try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        data = response.json()
-        return data.get("articles", [])
+        all_articles = newsapi.get_everything(
+            q=query, from_param=from_date, language="fr", sort_by="relevancy"
+        )
+        return all_articles
     except Exception as e:
         return {"error": str(e)}
+
+
+def search_tavily(query, days):
+    pass
 
 
 if __name__ == "__main__":
@@ -41,5 +50,8 @@ if __name__ == "__main__":
     user_query = sys.argv[1]
     days_to_search = sys.argv[2] if len(sys.argv) > 2 else 1
 
-    results = search_news(user_query, days_to_search)
-    print(json.dumps(results, indent=2))
+    results = search_newsapi(user_query, days_to_search)
+
+    sources = deduplicate_sources(results.get("articles", ""))
+    print(json.dumps(results, indent=2, ensure_ascii=False))
+    print(f"Doublons : {int(results.get('totalResults')) - len(sources)}")
